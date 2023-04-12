c15f957ad4c69ad75b673f716233aeb39916e82a
Diana Hrytsiuk <dian4kag80@gmail.com>
2023-04-12 18:58:12 +0300

    http

---
+++ main.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const https = require('https');
+//PDF Viewer потрібно
+const fileUrl = 'https://files.pidruchnyk.com.ua/uploads/book/11-klas-ukrajinska-mova-avramenko-2019.pdf'
+
+const fileName = 'READ.pdf'
+
+https.get(fileUrl, (res) => {
+  const file = fs.createWriteStream(fileName);
+  res.pipe(file);
+  
+  file.on('finish', () => {
+    console.log('Download complete.');
+    file.close();
+  });
+})
